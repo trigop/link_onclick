f1f0bcc525aea6317617d659659e99555f46a0c3
Paco Montes <pacomont@gmail.com>
2011-11-27 22:35:39 +0100

    cambios en el js

--- link_onclick.js
+++ link_onclick.js
@@ -1,6 +1,17 @@
 jQuery(document).ready(function(){
-  
+  var contadorSalvado = function(){
+    jQuery(console.log('Registrado'));
+  }
   jQuery('#block-views-bloque_los_mas_vistos-block a').click(function() {
-      jQuery(alert('hola'))
+      jQuery.ajax({
+        type: 'POST',
+        url: this.href,
+        dataType: 'json',
+        success: contadorSalvado,
+        data: 'js=1'
+      });
+      jQuery(console.log(this.href));
+      
+      return false;
       });  
 });
